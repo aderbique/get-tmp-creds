4d8941cdec369adf6fcf39c75687633c5980bc22
Austin Derbique <Austin.Derbique@Woven-Planet.global>
2024-08-30 12:51:46 -0700

    add support for multiple credentials

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='get-tmp-creds', 
-    version='0.3',
+    version='1.0',
     packages=find_packages(),
     install_requires=[
         'click',
